e0ccf5110d9429912e8adc8cb42eded6e8372d70
Ilie Todirascu <ilie.todirascu@amdaris.com>
2022-10-08 11:31:04 +0300

    Added Ciphers

---
+++ CryptographyLab1/CaesarPermutation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptographyLab1
+{
+    public class CaesarPermutation : CaesarCipher
+    {
+        public List<char> PermutatedAlphabet { get; set; } = new();
+        
+        public override string Encrypt(string text, Key<int> key)
+        {
+            PermutatedAlphabet = Utility.Alphabet.Select((x, i) => Utility.Alphabet[(i + key.Value) % 26]).ToList();
+            text = text.ToLower();
+            return new string(String.Join(" ", text.Split(" ").Select(x => new string(x.Select(y => PermutatedAlphabet[(Utility.Alphabet.FindIndex(c => c == y) + key.Value) % PermutatedAlphabet.Count]).ToArray())).ToArray()));
+        }
+        public override string Decrypt(string text, Key<int> key)
+        {
+            text = text.ToLower();
+            return new string(String.Join(" ", text.Split(" ").Select(x => new string(x.Select(y => PermutatedAlphabet[Utility.Mod(Utility.Alphabet.FindIndex(c => c == y) - key.Value, PermutatedAlphabet.Count)]).ToArray())).ToArray()));
+        }
+    }
+}
--- CryptographyLab1/Program.cs
+++ CryptographyLab1/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 
 namespace CryptographyLab1
 {
@@ -6,7 +13,22 @@ namespace CryptographyLab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var caesar = new CaesarCipher();
+            var viginere = new VigenereCipher();
+            var affine = new AffineCipher();
+            var permCaesar = new CaesarPermutation();
+            var word = caesar.Encrypt(Console.ReadLine(),new Key<int>(1));
+            Console.WriteLine(word);
+            Console.WriteLine(caesar.Decrypt(word));
+            word = Console.ReadLine();
+            Console.WriteLine(permCaesar.Encrypt(word,new Key<int>(1)));
+            Console.WriteLine(permCaesar.Decrypt(word, new Key<int>(1)));
+            var encryptedViginere = viginere.Encrypt("I am a FAF student", new Key<string>("cafe"));
+            Console.WriteLine(encryptedViginere);
+            Console.WriteLine(viginere.Decrypt(encryptedViginere, new Key<string>("cafe")));
+            var encryptedAffine = affine.Encrypt("AFFINE CIPHER", new Key<KeyValuePair<int, int>>(new KeyValuePair<int, int>(17,20)));
+            Console.WriteLine(encryptedAffine);
+            Console.WriteLine(affine.Decrypt(encryptedAffine, new Key<KeyValuePair<int, int>>(new KeyValuePair<int, int>(17,20))));
         }
     }
 }
